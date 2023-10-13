ed97e060d33c50c057d480478ebb7cf101884320
lightm103 <mchansws103@gmail.com>
2023-10-13 20:53:51 +0700

    comit email

--- resources/views/emails/pajakReminder.blade.php
+++ resources/views/emails/pajakReminder.blade.php
@@ -8,7 +8,7 @@
     <p>Tagihan Pajak Kendaraan Anda dengan Plat Nomor {{ $plat_nomer}} atas nama Sekretariat Daerah Kab.Kapuas Hulu</p>
     <p>Pajak kendaraan Anda akan berakhir pada {{ $tanggal_berakhir_pajak }}.</p>
     <p>Segera lunasi tagihan Pajak Kendaraan Anda.</p>
-    <p>Jika Anda membutuhkan bantuan, silakan menghubungi kami <br>melalui no whatsapp sebagai berikut : 0852-5234-3328</p>
+    <p>Jika anda  membutuhkan bantuan terkait kelengkapan administrasi untuk pembayaran pajak, silahkan menghubungi staf  bagian rumah tangga dan perlengkapan Sekretariat Daerah kab. Kapuas hulu</p>
     <h2>Abaikan notifikasi ini jika sudah melakukan pembayaran.</h2>
 </body>
 </html>
